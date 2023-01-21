811c8d977e7b8f3474990cc8e6f264f16f0e405b
techbeans-daisuke-yamashiro <daisuke.yamashiro@techbeans.co.jp>
2023-01-21 12:25:24 +0900

    - fix datadirectory oisition - added execution database initializer/seeder

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from database import create_db_and_tables,create_seed_data
 
 #initialize FastAPI
 app = FastAPI()
@@ -15,3 +16,10 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.on_event("startup")
+def init_app():
+    create_db_and_tables()
+    create_seed_data()
+
+if __name__ == '__main__':
+    init_app()
