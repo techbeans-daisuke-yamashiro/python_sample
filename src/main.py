fd74cc43b8d9ff223bf60e1d75c2975b88b0f364
techbeans-daisuke-yamashiro <daisuke.yamashiro@techbeans.co.jp>
2023-01-29 18:09:08 +0900

    added JWT Optional Endpoint

--- src/main.py
+++ src/main.py
@@ -19,12 +19,26 @@ app.include_router(LoginRouter)
 def read_root():
     return {"Hello": "World!"}
 
+# sample of JWT protected endpoint
+#  protected by Authorize.jwt_required()
 @app.get("/protected")
 def protected_contents(Authorize: AuthJWT = Depends()):
     Authorize.jwt_required()
     current_user = Authorize.get_jwt_subject()
     return current_user
 
+# sample of JWT protected(optional) endpoint
+#  protected by Authorize.jwt_optional()
+@app.get("/optional")
+def optional_protected_contents(Authorize: AuthJWT = Depends()):
+    res = {"message": "not logged in"}
+    Authorize.jwt_optional()
+    current_user = Authorize.get_jwt_subject()
+    if current_user:
+        res = {
+            "message":f"logged in {current_user}"
+        }
+    return res
 
 @app.on_event("startup")
 def init_app():
