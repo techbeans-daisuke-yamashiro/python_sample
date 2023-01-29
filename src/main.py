b851ba9a50d1055f5a2a660ef49838c7322e75eb
techbeans-daisuke-yamashiro <daisuke.yamashiro@techbeans.co.jp>
2023-01-29 20:09:43 +0900

    - Organized authorization errors into services.authorization

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi_jwt_auth import AuthJWT
 from database import create_db_and_tables, create_seed_data
 from errors import HttpRequestMiddleware
+from services.authorization import dispatch_jwt_authrization_error
 from config import DEBUG
 
 from routers import APIRoot
@@ -20,29 +21,22 @@ def read_root():
     return {"Hello": "World!"}
 
 # sample of JWT protected endpoint
-#  protected by Authorize.jwt_required()
+# protected by Authorize.jwt_required() 
+# dispatch Authrization Exception with dispatch_jwt_authrization_error() 
 @app.get("/protected")
 def protected_contents(Authorize: AuthJWT = Depends()):
-    Authorize.jwt_required()
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        dispatch_jwt_authrization_error(e)
     current_user = Authorize.get_jwt_subject()
     return current_user
-
-# sample of JWT protected(optional) endpoint
-#  protected by Authorize.jwt_optional()
-@app.get("/optional")
-def optional_protected_contents(Authorize: AuthJWT = Depends()):
-    res = {"message": "not logged in"}
-    Authorize.jwt_optional()
-    current_user = Authorize.get_jwt_subject()
-    if current_user:
-        res = {
-            "message":f"logged in {current_user}"
-        }
-    return res
 
 @app.on_event("startup")
 def init_app():
     create_db_and_tables()
 
 # add errorHandler with custom errors
-app.add_middleware(HttpRequestMiddleware)
+# if DEBUG=True in .env
+if not DEBUG:
+    app.add_middleware(HttpRequestMiddleware)
--- src/services/authorization/__init__.py
+++ src/services/authorization/__init__.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from fastapi_jwt_auth import AuthJWT
 
 from config import SECRET_KEY,ACCESS_TOKEN_EXPIRES_IN,REFRESH_TOKEN_EXPIRES_IN
+from errors import ApiException
 from passlib.context import CryptContext
 
 hash_password_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
@@ -42,3 +43,18 @@ class UserLoginResponse(BaseModel):
 @AuthJWT.load_config
 def get_config():
     return Settings()
+
+MissingTokenError = ApiException(status_code=401, message='You are not logged in')
+UserNotFoundError = ApiException(status_code=401, message='User no longer exist')
+NotVerifiedError = ApiException(status_code=401, message='Please verify your account')
+TokenExpiredError = ApiException(status_code=401, message='Token is invalid or has expired')
+
+def dispatch_jwt_authrization_error(e : Exception):
+    error = e.__class__.__name__
+    if error == 'MissingTokenError':
+        raise MissingTokenError
+    if error == 'UserNotFound':
+        raise UserNotFoundError
+    if error == 'NotVerified':
+        raise NotVerifiedError
+    raise TokenExpiredError
